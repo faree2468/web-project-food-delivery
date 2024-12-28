653aaf79d2965ae314806302540f4cddc81f14d5
faree2468 <faris.kadric@stu.ibu.edu.ba>
2024-12-28 12:50:14 +0100

    added routes for all pages

--- js/index.js
+++ js/index.js
@@ -10,7 +10,19 @@ app.route({
 
     view: "home",
     load: "main.html",
-    onCreate: function() {console.log("Home page created")},
+    onCreate: function() {
+        const btn = document.getElementById("nav-switchthemes")
+        if (btn) btn.addEventListener("click", switchThemes)
+
+        const faqs = document.querySelectorAll(".faq1");
+        faqs.forEach((item) => {
+            item.addEventListener("click", () => {
+                toggleFaq(item);
+            });
+        });
+
+        
+    },
     onReady: function() {
 
         setTimeout(() => homepagejs(), 0);
@@ -22,7 +34,7 @@ app.route({
 
     view:"recipe",
     load:"recipe.html",
-    onCreate: function() {console.log("Recipe page created")},
+    onCreate: function() {},
     onReady: function(){
 
         setTimeout(()=>recipejs(), 0)
@@ -44,75 +56,326 @@ app.route({
 
 })
 
-function homepagejs() {
+app.route({
+
+    view:"readmore",
+    load:"readmore.html",
+    onCreate: function(){console.log("Read more created")},
+    onReady: function(){
+        readmorejs()
+    }
+
+
+})
+
+app.route({
+
+    view:"leaderboard",
+    load:"leaderboards.html",
+    onCreate: function(){
+
+        
+        const table = document.getElementById("leaderboard-table")
+        var leaderboardData
+        async function getData() {
+            console.log("here")
+            try {
+
+                const res = await fetch("data/leaderboard.json")
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                const data = await res.json()
+            
+                leaderboardData = data
+                id = 0
+                data.forEach(person => {
+                    table.insertAdjacentHTML(
+                    "beforeend",
+                    `
+                    <tr>
+                        <td>${id}</td>
+                        <td>${person.name}</td>
+                        <td>${person.member_since}</td>
+                        <td>${person.total_amount_of_orders}</td>
+                        <td class="rremove" style="text-align: center; color: red; cursor: pointer;"><i class="fa-solid fa-x"></i></td>
+                    </tr>
+                    `
+                    );
+                    id+=1
+                });
+
+                addListeners()
+
+            } catch (error) {
+
+                console.error("Error fetching leaderboard data:", error);
+
+            }
+            
+
+        }
+
+        getData()
+
+    },
+    onReady: function(){
+
+        leaderboardjs()
+
+    }
+
+
+})
+
+app.route({
+
+    view:"login",
+    load:"login.html",
+    onCreate: function(){console.log("this is created")},
+    onReady: function(){
+        setTimeout(()=>loginjs(), 0)
+    }
+
+})
+
+function addListeners() {
+
+    const removebtns = document.querySelectorAll(".rremove")
+
+    removebtns.forEach((item)=> {
+
+        item.addEventListener("click", ()=>{
+
+            removeRow(item)
+
+        })
+
+    })
+
+}
+
+function removeRow(btn) {
+
+    const row = btn.closest("tr");
+    if (row) {
+        row.remove();
+    }
+
+}
+
+function leaderboardjs() {
+
+    // LEADERBOARD
+    const table = document.getElementById("leaderboard-table")
+
+    toastr.options = {
+        closeButton: true,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        timeOut: "3000"
+    };
+
+
+    const editForm = document.getElementById("editForm")
+    editForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+        const idToFind = document.getElementById("iid").value.trim() // removing whitespace for id
+        const name = document.getElementById("name").value
+        const accInfo = document.getElementById("account-created").value
+        const total = document.getElementById("total-amount").value
+        
+        for (let i = 1; i < table.rows.length; i++) {
+            let row = table.rows[i]
+            let rowId = row.cells[0].textContent.trim()
+
+            
+            if (rowId === idToFind) {
+                
+                row.cells[1].textContent = name
+                row.cells[2].textContent = accInfo
+                row.cells[3].textContent = total
+                toastr.success(`ID: ${idToFind} is updated!`)
+                editForm.reset()
+                return
+            }
+        }
+
+        toastr.error(`No row with ID: ${idToFind}.`)
+    })
+
+
+    // IMAGE GALLERY
+    const modal = document.getElementById("myModal")
+
+    const modalImg = document.getElementById("img")
+
+
+    imgs = document.querySelectorAll(".image-css")
+
+    imgs.forEach((img)=>{
+
+        img.onclick = function() {
+            
+            modal.style.display = "block"
+            modalImg.src = this.src
+
+        }
+
+    })
+
+    const span = document.querySelector(".close-gallery")
+    span.onclick = function() { 
+
+    modal.style.display = "none"
+
+    }
+
+    window.onclick = function(event) {
+
+        if (event.target == modal) {
+
+            modal.style.display = "none"
+
+        }
+
+    }
+
+
+
+
     // DARK MODE
-    const btn = document.getElementById("nav-switchthemes");
-    const iconbtn = document.getElementById("iconbtn");
-    const links = document.getElementsByTagName("a");
-    const btns = document.getElementsByTagName("button");
-    const spans = document.getElementsByTagName("span");
+    var links = document.getElementsByTagName("a")
+    var btns = document.getElementsByTagName("button")
+    var spans = document.getElementsByTagName("span")
+
+    var cart = document.querySelector(".cart-total")
 
     function darkMode() {
-        iconbtn.classList.remove("fa-moon");
-        iconbtn.classList.add("fa-sun");
-        for (let link of links) link.classList.add("dark-mode-a");
-        for (let btn of btns) btn.classList.add("dark-mode-btn");
-        for (let span of spans) {
-            if (span.classList.contains("item-text")) span.classList.add("dark-mode-span");
+
+        for (let link of links) {
+                
+            link.classList.add("dark-mode-a")
+
+
         }
+
+        for (let btn of btns) {
+
+            btn.classList.add("dark-mode-btn")
+
+        }
+
+        for (let span of spans) {
+
+            if (span.classList.contains("item-text")) {
+
+                span.classList.add("dark-mode-span")
+            }
+        }
+
+
     }
 
     function lightMode() {
-        iconbtn.classList.remove("fa-sun");
-        iconbtn.classList.add("fa-moon");
-        for (let link of links) link.classList.remove("dark-mode-a");
-        for (let btn of btns) btn.classList.remove("dark-mode-btn");
-        for (let span of spans) {
-            if (span.classList.contains("item-text")) span.classList.remove("dark-mode-span");
+
+        
+        for (let link of links) {
+            
+            link.classList.remove("dark-mode-a")
+            
         }
+
+        for (let btn of btns) {
+
+            btn.classList.remove("dark-mode-btn")
+
+        }
+
+        for (let span of spans) {
+
+            if (span.classList.contains("item-text")) {
+
+                span.classList.remove("dark-mode-span")
+            }
+        }
+
     }
 
-    function switchThemes() {
-        const elements = document.body;
-        elements.classList.toggle("dark-mode");
-        if (elements.classList.contains("dark-mode")) {
-            localStorage.setItem("theme", "dark");
+    document.addEventListener("DOMContentLoaded", () => {
+
+        if(localStorage.getItem("theme") === "dark") {
+
+            document.body.classList.add("dark-mode");
             darkMode();
+
         } else {
-            localStorage.setItem("theme", "white");
+            document.body.classList.remove("dark-mode");
             lightMode();
         }
-    }
 
-    
+    })
+
+}
+
+const iconbtn = document.getElementById("iconbtn");
+const links = document.getElementsByTagName("a");
+const btns = document.getElementsByTagName("button");
+const spans = document.getElementsByTagName("span");
+
+function switchThemes() {
+    const elements = document.body;
+    elements.classList.toggle("dark-mode");
+    if (elements.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        darkMode();
+    } else {
+        localStorage.setItem("theme", "white");
+        lightMode();
+    }
+}
+
+function darkMode() {
+    iconbtn.classList.remove("fa-moon");
+    iconbtn.classList.add("fa-sun");
+    for (let link of links) link.classList.add("dark-mode-a");
+    for (let btn of btns) btn.classList.add("dark-mode-btn");
+    for (let span of spans) {
+        if (span.classList.contains("item-text")) span.classList.add("dark-mode-span");
+    }
+}
+
+function lightMode() {
+    iconbtn.classList.remove("fa-sun");
+    iconbtn.classList.add("fa-moon");
+    for (let link of links) link.classList.remove("dark-mode-a");
+    for (let btn of btns) btn.classList.remove("dark-mode-btn");
+    for (let span of spans) {
+        if (span.classList.contains("item-text")) span.classList.remove("dark-mode-span");
+    }
+}
+
+function toggleFaq(faq) {
+    const ans = faq.nextElementSibling;
+    if (ans.style.display === "block") {
+        ans.style.display = "none";
+    } else {
+        document.querySelectorAll(".ans1").forEach((ansItem) => {
+            ansItem.style.display = "none";
+        });
+        ans.style.display = "block";
+    }
+}
+
+function homepagejs() {
+    // DARK MODE
     if (localStorage.getItem("theme") === "dark") {
         document.body.classList.add("dark-mode");
         darkMode();
     } else {
         document.body.classList.remove("dark-mode");
         lightMode();
-    }
-
-    if (btn) btn.addEventListener("click", switchThemes);
-
-    // FAQ ACCORDION
-    const faqs = document.querySelectorAll(".faq1");
-    faqs.forEach((item) => {
-        item.addEventListener("click", () => {
-            toggleFaq(item);
-        });
-    });
-
-    function toggleFaq(faq) {
-        const ans = faq.nextElementSibling;
-        if (ans.style.display === "block") {
-            ans.style.display = "none";
-        } else {
-            document.querySelectorAll(".ans1").forEach((ansItem) => {
-                ansItem.style.display = "none";
-            });
-            ans.style.display = "block";
-        }
     }
 }
 
@@ -230,16 +493,7 @@ function recipejs() {
             lightMode();
         }
 
-        // READ MORE
-
-        readmore = document.getElementById("readmore")
-
-        readmore.addEventListener("click", ()=>{
-
-            
-            location.href = 'readmore.html'
-
-        })
+        
 
     })
 
@@ -285,6 +539,153 @@ function cartjs() {
     function lightMode() {
 
         cart.style.background = "lightgrey"
+        for (let link of links) {
+            
+            link.classList.remove("dark-mode-a")
+            
+        }
+
+        for (let btn of btns) {
+
+            btn.classList.remove("dark-mode-btn")
+
+        }
+
+        for (let span of spans) {
+
+            if (span.classList.contains("item-text")) {
+
+                span.classList.remove("dark-mode-span")
+            }
+        }
+
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+        if(localStorage.getItem("theme") === "dark") {
+
+            document.body.classList.add("dark-mode");
+            darkMode();
+
+        } else {
+            document.body.classList.remove("dark-mode");
+            lightMode();
+        }
+
+    })
+
+}
+
+function readmorejs() {
+
+    var links = document.getElementsByTagName("a")
+    var btns = document.getElementsByTagName("button")
+    var spans = document.getElementsByTagName("span")
+
+    function darkMode() {
+
+        for (let link of links) {
+                
+            link.classList.add("dark-mode-a")
+
+
+        }
+
+        for (let btn of btns) {
+
+            btn.classList.add("dark-mode-btn")
+
+        }
+
+        for (let span of spans) {
+
+            if (span.classList.contains("item-text")) {
+
+                span.classList.add("dark-mode-span")
+            }
+        }
+
+
+    }
+
+    function lightMode() {
+
+        
+        for (let link of links) {
+            
+            link.classList.remove("dark-mode-a")
+            
+        }
+
+        for (let btn of btns) {
+
+            btn.classList.remove("dark-mode-btn")
+
+        }
+
+        for (let span of spans) {
+
+            if (span.classList.contains("item-text")) {
+
+                span.classList.remove("dark-mode-span")
+            }
+        }
+
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+        if(localStorage.getItem("theme") === "dark") {
+
+            document.body.classList.add("dark-mode");
+            darkMode();
+
+        } else {
+            document.body.classList.remove("dark-mode");
+            lightMode();
+        }
+
+    })
+
+}
+
+function loginjs() {
+
+    var links = document.getElementsByTagName("a")
+    var btns = document.getElementsByTagName("button")
+    var spans = document.getElementsByTagName("span")
+
+    function darkMode() {
+
+        cart.style.background = "black"
+        for (let link of links) {
+                
+            link.classList.add("dark-mode-a")
+
+
+        }
+
+        for (let btn of btns) {
+
+            btn.classList.add("dark-mode-btn")
+
+        }
+
+        for (let span of spans) {
+
+            if (span.classList.contains("item-text")) {
+
+                span.classList.add("dark-mode-span")
+            }
+        }
+
+
+    }
+
+    function lightMode() {
+
+        
         for (let link of links) {
             
             link.classList.remove("dark-mode-a")
